72a3641e68b9e1f152ac61ae6b13a6b2bd6131dd
murugabalu-sivakumar <murugabalu.sivakumar@gmail.com>
2024-03-03 23:15:35 -0500

    Practice string problem

---
+++ src/strings/SpecialSequence.java
@@ -0,0 +1,29 @@
+package strings;
+
+public class SpecialSequence {
+
+  public int solve(String A) {
+
+    int MOD = 1000 * 1000 * 1000 + 7;
+    int aCount = 0, res = 0;
+
+    for (int i = 0; i < A.length(); i++) {
+        if (A.charAt(i) == 'A') {
+            aCount++;
+        } else if (A.charAt(i) == 'G') {
+            res += aCount;
+            res = res % MOD;
+        }
+    }
+
+    return res;
+  }
+}
+
+/*
+You have given a string A having Uppercase English letters.
+
+You have to find how many times subsequence "AG" is there in the given string.
+
+NOTE: Return the answer modulo 109 + 7 as the answer can be very large.
+ */
