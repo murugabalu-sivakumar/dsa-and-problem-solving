47deae2c01efd1c51c58056e765610e07f8262b8
murugabalu-sivakumar <murugabalu.sivakumar@gmail.com>
2024-03-27 21:58:12 -0400

    Create a folder separately for Subarrays

--- src/arrays/subarray/CountSubarrays.java
+++ src/arrays/subarray/CountSubarrays.java
@@ -1,4 +1,4 @@
-package arrays;
+package arrays.subarray;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- src/arrays/subarray/MaximumSubarray.java
+++ src/arrays/subarray/MaximumSubarray.java
@@ -1,4 +1,4 @@
-package arrays;
+package arrays.subarray;
 
 import java.util.ArrayList;
 
